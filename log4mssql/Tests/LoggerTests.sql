02d93c93503fa05ed1ddf6edf513879a35e21d99
jbpion <jbpion@gmail.com>
2018-01-13 22:05:55 -0600

    Add test for LoggerBase.Config_Layout

--- log4mssql/Tests/LoggerTests.sql
+++ log4mssql/Tests/LoggerTests.sql
@@ -266,6 +266,31 @@ BEGIN
 END;
 GO
 
+CREATE PROCEDURE loggerbasetests.[Test Assert Function Config_Layout Returns The Layout Type And XML Config]
+AS
+BEGIN
+
+	DECLARE @TestConfig XML = '
+	<appender name="A1" type="LoggerBase.Appender_ConsoleAppender">
+        <layout type="LoggerBase.Layout_PatternLayout">
+            <conversionPattern value="%-4timestamp [%thread] %-5level %logger %ndc - %message%newline"/>
+        </layout>
+    </appender>'
+	DECLARE @ExpectedLayoutType   VARCHAR(500)  = 'LoggerBase.Layout_PatternLayout'
+	DECLARE @ExpectedLayoutConfig VARCHAR(4000) = '<layout type="LoggerBase.Layout_PatternLayout"><conversionPattern value="%-4timestamp [%thread] %-5level %logger %ndc - %message%newline"/></layout>'
+
+	DECLARE @ActualLayoutType   VARCHAR(500) 
+	DECLARE @ActualLayoutConfig VARCHAR(4000)
+
+	SELECT @ActualLayoutType = LayoutType, @ActualLayoutConfig = CONVERT(VARCHAR(4000), LayoutConfig)
+	FROM LoggerBase.Config_Layout(@TestConfig)
+
+	EXEC tSQLt.AssertEquals @Expected = @ExpectedLayoutType, @Actual = @ActualLayoutType
+	EXEC tSQLt.AssertEquals @Expected = @ExpectedLayoutConfig, @Actual = @ActualLayoutConfig
+
+END;
+GO
+
 
 EXEC tSQLt.Run 'loggerbasetests'
 GO
