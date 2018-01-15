41c3dad8c3cb428e904d4549c18edf36bb68af09
jbpion <jbpion@gmail.com>
2018-01-15 12:59:39 -0600

    Add test for Config_Root_Get jbpion

--- log4mssql/Tests/LoggerTests.sql
+++ log4mssql/Tests/LoggerTests.sql
@@ -312,7 +312,33 @@ BEGIN
 END;
 GO
 
+CREATE PROCEDURE loggerbasetests.[Test Assert We Can Get The Root Config XML]
+AS
+BEGIN
+
+	DECLARE @Config XML = '<log4mssql>
+    <root>
+        <level value="INFO" />
+        <appender-ref ref="Appender1" />
+		<appender-ref ref="Appender2" />
+    </root>
+</log4mssql>'
+
+	SELECT RowID = 1, LevelValue = 'INFO', AppenderRef = 'Appender1'
+	INTO #Expected
+	UNION ALL
+	SELECT RowID = 2, LevelValue = 'INFO', AppenderRef = 'Appender2'
+
+	SELECT RowID, LevelValue, AppenderRef 
+	INTO #Actual
+	FROM LoggerBase.Config_Root_Get(@Config)
+
+	EXEC tSQLt.AssertEqualsTable @Expected = '#Expected', @Actual = '#Actual'
+
+END;
+GO
+
 EXEC tSQLt.Run 'loggerbasetests'
---EXEC tSQLt.Run 'loggerbasetests.[Test Assert We Can Get The Current Session (ContextID) Config XML]'
+--EXEC tSQLt.Run 'loggerbasetests.[Test Assert We Can Get The Root Config XML]'
 GO
 
